7471474d09b70fdd313ae62868aa357ad9eaae61
KoenVA <s144435@ap.be>
2025-10-07 12:30:49 +0200

    Add ASP.NET Web API project

--- AP.CityDataDemo.BlazorApp/Program.cs
+++ AP.CityDataDemo.BlazorApp/Program.cs
@@ -7,9 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.RegisterApplication();
-builder.Services.RegisterInfrastructure();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ AP.CityDataDemo.Presentation/Program.cs
@@ -0,0 +1,25 @@
+using AP.CityDataDemo.Application.Extensions;
+using AP.CityDataDemo.Infrastructure.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.RegisterApplication();
+builder.Services.RegisterInfrastructure();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
