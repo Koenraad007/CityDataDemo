1b260b45279fb4c765fd79b7808a7c14920210c0
KoenVA <s144435@ap.be>
2025-10-07 12:49:39 +0200

    Add controllers

---
+++ AP.CityDataDemo.Presentation/Controllers/CityController.cs
@@ -0,0 +1,66 @@
+using AP.CityDataDemo.Application.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AP.CityDataDemo.Presentation.Controllers
+{
+    public class CityController : APIv1Controller
+    {
+        private readonly ICityService _cityService;
+        private readonly IMediator _mediator;
+
+        public CityController(ICityService cityService, IMediator mediator)
+        {
+            _cityService = cityService ?? throw new ArgumentNullException(nameof(cityService));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCities()
+        {
+            var cities = await _cityService.GetCitiesAsync();
+            return Ok(cities);
+        }
+
+        [HttpGet("{cityId}")]
+        public async Task<IActionResult> GetCity(int cityId, bool includePointsOfInterest = false)
+        {
+            var city = await _cityService.GetCityByIdAsync(cityId, includePointsOfInterest);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            return Ok(city);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCity([FromBody] Application.DTOs.CityDto city)
+        {
+            var createdCity = await _cityService.CreateCityAsync(city);
+            return CreatedAtAction(nameof(GetCity), new { cityId = createdCity.Id }, createdCity);
+        }
+
+        [HttpPut("{cityId}")]
+        public async Task<IActionResult> UpdateCity(int cityId, [FromBody] Application.DTOs.CityDto city)
+        {
+            var updated = await _cityService.UpdateCityAsync(cityId, city);
+            if (!updated)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{cityId}")]
+        public async Task<IActionResult> DeleteCity(int cityId)
+        {
+            var deleted = await _cityService.DeleteCityAsync(cityId);
+            if (!deleted)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+
+    }
+}
---
+++ AP.CityDataDemo.Presentation/Controllers/CountryController.cs
@@ -0,0 +1,65 @@
+using AP.CityDataDemo.Application.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AP.CityDataDemo.Presentation.Controllers
+{
+    public class CountryController : APIv1Controller
+    {
+        private readonly ICountryService _countryService;
+        private readonly IMediator _mediator;
+
+        public CountryController(ICountryService countryService, IMediator mediator)
+        {
+            _countryService = countryService;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetCountries()
+        {
+            var countries = await _countryService.GetCountriesAsync();
+            return Ok(countries);
+        }
+
+        [HttpGet("{countryId}")]
+        public async Task<ActionResult> GetById(int countryId, bool includeCities = false)
+        {
+            var country = await _countryService.GetCountryByIdAsync(countryId, includeCities);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] CountryDto country)
+        {
+            var createdCountry = await _countryService.CreateCountryAsync(country);
+            return CreatedAtAction(nameof(GetById), new { countryId = createdCountry.Id }, createdCountry);
+        }
+
+        [HttpPut("{countryId}")]
+        public async Task<ActionResult> Update(int countryId, [FromBody] CountryDto country)
+        {
+            var updated = await _countryService.UpdateCountryAsync(countryId, country);
+            if (!updated)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{countryId}")]
+        public async Task<ActionResult> Delete(int countryId)
+        {
+            var deleted = await _countryService.DeleteCountryAsync(countryId);
+            if (!deleted)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+    }
+}
